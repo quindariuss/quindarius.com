390d15095f865c10c60da381ab5f07077a6e8785
quiwoodz@gmail.com <quiwoodz@gmail.com>
2022-01-06 03:31:07 -0500

    looks like the modfied date is shown instead of created date

--- blog-page.sh
+++ blog-page.sh
@@ -1,2 +1,2 @@
-ls -lhsU blog/text |awk '$1 != "total" && $9 != "blog-post.sh" {  printf("<a href=\"blog/html/%s\"> %s %s -- %s</a><br/>\n", $9,$6, $7, $9)}' | sed 's/.txt/.html/g' > links.html
+ls -lhsU blog/text |awk '$1 != "total" && $10 != "blog-post.sh" {  printf("<a href=\"blog/html/%s\"> %s %s -- %s</a><br/>\n", $10,$7, $8, $10)}' | sed 's/.txt/.html/g' > links.html
 cat templates/header.html templates/tags/main.html links.html templates/tags/mmain.html > blog.html
