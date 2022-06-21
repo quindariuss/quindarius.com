0c85d45b89eed8a50133e94d8990196b043b637a
Quin'darius Lyles-Woods <qlyleswoods@rekor.ai>
2022-06-21 23:15:44 +0000

    Start of the What’s New in CoreML Talk

--- blog-page.sh
+++ blog-page.sh
@@ -1,2 +1,4 @@
-ls -t blog/text |awk '$1 != "total" && $1 != "blog-post.sh" {  printf("<a href=\"blog/html/%s\">%s</a><br/>\n",$1,$1 )}' | sed 's/.txt/.html/g'  > links.html
+ls -t blog/text |
+awk '$1 != "total" && $1 != "blog-post.sh" {  printf("<a href=\"blog/html/%s\">%s</a><br/>\n",$1,$1 )}' | 
+sed 's/.txt/.html/g'  > links.html
 cat templates/header.html templates/tags/main.html links.html templates/tags/mmain.html > blog.html
