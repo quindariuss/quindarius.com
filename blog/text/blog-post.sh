f135e9dfeba21217ca30a8dabe9bbabfb96ffad8
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2022-10-14 02:52:56 +0000

    purge

--- blog/text/blog-post.sh
+++
@@ -1,6 +0,0 @@
-for index in *;
-    do
-        cat ../../templates/blog-header.html ../../templates/tags/pre.html $index ../../templates/tags/ppre.html > ../html/${index%.*}.html ;
-done;
-
-rm ../html/blog-post.html 
